a0835d963541c2bc96ffc7707b6597013e3ab07b
pupipatsk <pupipat.sk@gmail.com>
2025-12-24 15:35:36 +0700

    favico

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -60,10 +60,14 @@ export const metadata: Metadata = {
     images: ["/profile/profile-photo.png"],
   },
   icons: {
-    icon: "/profile/profile-photo.png",
-    shortcut: "/profile/profile-photo.png",
-    apple: "/profile/profile-photo.png",
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
   },
+  manifest: "/site.webmanifest",
 };
 
 export const viewport: Viewport = {
